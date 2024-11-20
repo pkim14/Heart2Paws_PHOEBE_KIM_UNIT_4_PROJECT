c2c3308f83f6a06e60bf42001145c02399af32d7
Phoebe Kim <phoebe19kim@gmail.com>
2024-11-20 10:37:19 -0500

    I finished my RescueCenter class and CareTaker class. I started working on my main class, but noticed problems :(
    I edited all my object files so that there are no more print statements in them. I added all the print statements to my main file. I also noticed that I don't have any user inputs, so I started adding some in. :)
    
    I finished adding all the user input statements, but found an issue while running the code. I started to fix the issue, but I need to finish. :(
    
    I was trying to fix my RescueCenter class because there's an issue with the user trying feed the animal medicine. :(
    
    Finally fixed my RescueCenter class & edited my Animal class so that the animals don't get released really fast. :)
    
    Added my javadoc, still needs to be finished! :)
    
    Finished my javadoc! :)

--- src/CareTaker.java
+++ src/CareTaker.java
@@ -1,12 +1,29 @@
+/**
+ * Represents a caretaker responsible for providing care to animals in the rescue center
+ */
 public class CareTaker {
     private String name;
     private int energy;
 
+    /**
+     * Constructs a caretaker with a name & initial energy level
+     *
+     * @param name represents the name of the caretaker
+     * @param energy represents the initial energy level of the caretaker
+     */
     public CareTaker(String name, int energy) {
         this.name = name;
         this.energy = energy;
     }
 
+    /**
+     * Provides care to an animal if the caretaker has energy
+     *
+     * @param center represents the rescue center where care is being provided
+     * @param careType represents the type of care to be given (food or medical)
+     * @param animal represents the animal receiving care
+     * @return true if care is successfully provided, false otherwise
+     */
     public boolean careForAnimal(RescueCenter center, String careType, Animal animal) {
         if (energy > 0) {
             boolean success = center.provideCare(careType, animal);
@@ -18,11 +35,18 @@ public class CareTaker {
         return false;
     }
 
-
+    /**
+     * Allows the caretaker to rest & regain energy
+     */
     public void rest() {
         energy += 5;
     }
 
+    /**
+     * Gets the current energy level of the caretaker
+     *
+     * @return current energy level
+     */
     public int getEnergy() {
         return energy;
     }
